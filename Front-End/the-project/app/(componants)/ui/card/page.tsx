21e5e869c387f7b56c4c699ed7d3b82813f467fd
Ahmed Eissa <aethefifthofjuly@gmail.com>
2026-01-02 15:34:32 +0200

    Update page.tsx

--- Front-End/the-project/app/(componants)/ui/card/page.tsx
+++ Front-End/the-project/app/(componants)/ui/card/page.tsx
@@ -1,15 +1,1 @@
-export function Card({ children, className }) {
-  return (
-    <div className={`rounded-2xl bg-white dark:bg-gray-800 ${className}`}>
-      {children}
-    </div>
-  );
-}
 
-export function CardContent({ children, className }) {
-  return (
-    <div className={`p-4 ${className}`}>
-      {children}
-    </div>
-  );
-}
